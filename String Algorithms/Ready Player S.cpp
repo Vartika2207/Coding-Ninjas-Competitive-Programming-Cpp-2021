13cbab859c03d6184544f4728022189db55e3c84
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2021-11-13 06:39:37 +0530

    Update Ready Player S.cpp

--- String Algorithms/Ready Player S.cpp
+++ String Algorithms/Ready Player S.cpp
@@ -1,17 +1,31 @@
+/*****************
+Ready Player S?
+
+OASIS is a virtual reality created by the legendary James Halliday. After Halliday's death, a pre-recorded message left by him announced a game, which would grant the ownership and control of the OASIS to the first player who finds the Golden Easter Egg within it.
+Shivali, an OASIS player, is obsessed with the game and finding the Easter Egg. But she has to fight the IOI clan, who wants to control OASIS for evil purposes. Both of them gather troops of different types and form a big army to fight each other.
+IOI has N troops of lowercase letters forming a huge army. Shivali has an army of length M.
+She will win, if the first k troops she takes from her army, can kill any of the k consecutive troops of the IOI army.
+Remember a troop of type 'a' can only kill a troop of type 'a'.
+You have to find how many times she can win.
+Input Format:
+The first line of input contains N, M and k, space separated.
+Next two lines contains the string of troops of length N and M respectively in lowercase letters.
+Constraints:
+1 <= N, M <= 10^6
+1 <= K <= M
+Output Format:
+Output the number of wins she is going to take at the end of the day. Print -1 if she can't win.
+Sample Input 1:
+3 2 1
+bbb
+bb
+Sample Output 1:
+3
+********************/
+
 #include<bits/stdc++.h>
 using namespace std;
-// int main(){
-    
-//     int t;
-//     t = 1;
-//     while(t--){
-//         int n, m, k; // IOI has N troops of lowercase letters forming a huge army. Shivali has an army of length M.
-//         cin >> n >> m >> k;
-//         string ioiTroop, shivaliArmy;
-//         cin >> ioiTroop >> shivaliArmy;
-//     }
-//     return 0;
-// }
+
 void checker(int p_len, string &pattern, vector<int> &v)
 {
     int j = 0;

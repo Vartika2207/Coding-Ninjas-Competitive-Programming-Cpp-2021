eac8bfa97a33031e26ea1d133aed719ee9a5584d
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2021-11-13 22:47:04 +0530

    Update Ninja and Multiset.cpp

--- Tries/Ninja and Multiset.cpp
+++ Tries/Ninja and Multiset.cpp
@@ -1,3 +1,45 @@
+/***************
+Ninja and Multiset
+
+Our problem setter, Ninja, is fascinated by different types of sets. Quite recently, he came across a new term- Multiset. Multiset is a set, where equal elements are allowed. He started experimenting with it and has managed to frame a pretty interesting problem for you to solve.
+You are given a multiset G, initially containing only 0, and a few queries q. Queries are actually of 3 types:
+"+ x" — This query adds an integer x to the given multiset.
+"- x" — This query erases one occurrence of x from the given multiset. Before this query is encountered, it is guaranteed that the multiset will contain integer x at least once.
+"? x" — In this query, you are given integer x and you need to compute the maximum value of bitwise exclusive OR (also known as XOR) of integer x and some integer y from the given multiset.
+Input Format:
+First line on input contain Q (number of queries ).
+Next Q line contain Q queries as defined above.
+Output Format:
+For each query of type '?' print the answer in new line
+Constraints:
+1 <= Q <= 10^5
+1 <= x <= 10^9
+Sample Input 1:
+8
+? 8
+? 5
++ 10
+? 5
+? 4
++ 4
+- 4
+? 10
+Sample Output 1:
+8
+5
+15
+14
+10
+Explanation:
+Initially the multiset contains only the integer 0. So for the first query, answer is 8 XOR 0 = 8.
+Similarily, the answer for second query is 5 XOR 0 = 5.
+After the third query, 10 is added to the multiset.
+For the fourth query, 5 will be XOR'ed with 0 and 10. Out of these, 5 XOR 10 = 15 is max.
+Similarily, for the fifth query, 4 will be XOR'ed with 0 and 10. Out of these, 4 XOR 10 = 14 is max.
+After the sixth query, 4 is added to the multiset.
+After the seventh query, 4 is erased from the multiset.
+For the last query, 10 is XOR'ed with 0 and 10. Out of these, 10 XOR 0 = 10 is max.
+****************/
 #include<bits/stdc++.h>
 using namespace std;
 

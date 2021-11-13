c304858b103ce10d44f44833b6da8b894ab90a98
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2021-11-13 17:40:08 +0530

    Update Search Engine.cpp

--- Tries/Search Engine.cpp
+++ Tries/Search Engine.cpp
@@ -1,3 +1,27 @@
+/******************
+Search Engine
+
+Let us see how search engines work. Consider the following simple auto complete feature. When you type some characters in the text bar, the engine automatically gives best matching options among it's database. Your job is simple. Given an incomplete search text, output the best search result.
+Each entry in engine's database has a priority factor attached to it. We consider a result / search suggestion best if it has maximum weight and completes the given incomplete search query. For each query in the input, print the maximum weight of the string in the database, that completes the given incomplete search string. In case no such string exists, print -1.
+Input Format:
+First line contains two integers n and q, which represent number of database entries and number of search queries need to be completed. 
+Next n lines contain a string s and an integer weight, which are the database entry and it's corresponding priority.
+Next q lines follow, each line having a string t, which needs to be completed.
+Constraints:
+1 ≤ n, weight, len(s), len(t) ≤ 10^6
+1 ≤ q ≤ 10^5
+Total length of all strings in database entries ≤ 10^6
+Total length of all query strings ≤ 10^6
+Output Format:
+Output q lines, each line containing the maximum possible weight of the match for given query, else -1, in case no valid result is obtained.
+Sample Input 1:
+2 1
+potential 10
+potent 8
+pot
+Sample Output 1:
+10
+******************/
 #include<bits/stdc++.h>
 using namespace std;
 
